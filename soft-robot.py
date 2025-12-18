6bfc8cb0bb67fd5c050b095b1281cf57cf18e33d
Rishaan Nandan <poulomi.nandan@gmail.com>
2025-12-17 20:53:13 -0500

    Create soft-robot.py

---
+++ soft-robot.py
@@ -0,0 +1,21 @@
+from ursina import *
+import math
+
+class SoftRobot(Entity):
+    def __init__(self, position=(0,0,0), **kwargs):
+        super().__init__()
+        self.model = 'cube'
+        self.color = color.orange
+        self.scale_y = 1.5
+        self.position = position
+        self.flex_intensity = 0.2
+        self.data_collected = 0
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def update(self):
+        # Soft motion (squash/stretch)
+        self.scale_y = 1.5 + math.sin(time.time()) * self.flex_intensity
+        # Collect data over time
+        self.data_collected += 0.01 * time.dt
