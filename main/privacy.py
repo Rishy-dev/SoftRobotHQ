79f04f759b94d1012a247ebfb50c478096382e6f
Rishaan Nandan <poulomi.nandan@gmail.com>
2025-12-17 21:01:15 -0500

    Update privacy.py

--- main/privacy.py
+++ main/privacy.py
@@ -1,9 +1,13 @@
+import time
+
 class PrivacySystem:
     def __init__(self):
         self.weakness = 10  # 0 = secure, 100 = very weak
-        self.growth_rate = 0.5
+        self.growth_rate = 0.3
 
-    def update(self):
-        self.weakness += self.growth_rate * time.dt
+    def update(self, robots):
+        # Increase based on number of robots and their data collection
+        robot_factor = sum([r.data_collected for r in robots])
+        self.weakness += self.growth_rate * time.dt + robot_factor * 0.1
         if self.weakness > 100:
             self.weakness = 100
